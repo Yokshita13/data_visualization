49f4c88daccc288055a894ee83b5c6bc643098e6
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2025-05-15 14:14:06 +0530

    Add files via upload

---
+++ checking_missingData.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+data = {
+    'Name' : ['Yokshita','Lakshya',None,'Meera','Kabir','Ishita'],
+    'ID' : [1,2,3,None,5,6]
+}
+
+df = pd.DataFrame(data)
+
+print(df.isnull().sum()) # Total missing values per column
+
+print(df.isnull().any()) # Checks if any column has missing values
