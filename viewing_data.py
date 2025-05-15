00c79d8c9424b6be5afff14fced2531918209352
Yokshita Jaiswal <yokshitajaiswal@gmail.com>
2025-05-15 13:55:56 +0530

    Update viewing_data.py

--- viewing_data.py
+++ viewing_data.py
@@ -3,6 +3,5 @@ import matplotlib.pyplot as plt
 
 df= pd.read_csv("css/project/DV/Student_performance_dataset.csv")
 
-# print(df.head(52))
-print(df.tail(25))
-
+print(df.head(48)) #display first n rows
+print(df.tail(25)) #display last n rows
